6f54513b561430854d6d13913b71436be676cdf8
Stuart Whelan <109048764+stuart-storypark@users.noreply.github.com>
2023-11-10 10:15:18 +1300

    Update build_environment.sh

--- scripts/python/build_environment.sh
+++ scripts/python/build_environment.sh
@@ -16,7 +16,7 @@ if ! [ -d $WORK_DIR ]; then
   exit 1
 fi
 
-PYTHON_VERSIONS="python2.7 python3.6 python3.7 python3.8"
+PYTHON_VERSIONS="python2.7 python3.6 python3.7 python3.8 python3.11"
 
 if ! [[ $PYTHON_VERSIONS =~ (^|[[:space:]])$PYTHON_RUNTIME($|[[:space:]]) ]]; then
   echo "ERROR: Invalid python runtime $PYTHON_RUNTIME"
